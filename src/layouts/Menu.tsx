b5c4227fb487e7410a43d5aa4c712d47bf9d77fc
sonlha <lhas1307@gmail.com>
2022-03-09 17:06:54 +0700

    change menu icon

--- src/layouts/Menu.tsx
+++ src/layouts/Menu.tsx
@@ -1,25 +1,44 @@
-import { Button, Typography } from 'antd';
-import { ReactComponent as Logo } from 'assets/icons/logo.svg';
-import { ThemeContext, Themes } from 'context/theme-context';
-import React, { memo, useContext, useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import styles from './Menu.module.scss';
+import { Button, Typography } from "antd";
+import { ReactComponent as Logo } from "assets/icons/logo.svg";
+import { ReactComponent as Swap } from "assets/icons/swap.svg";
+import { ReactComponent as Wallet } from "assets/icons/wallet.svg";
+import { ReactComponent as Pools } from "assets/icons/pool.svg";
+import { ThemeContext, Themes } from "context/theme-context";
+import React, {
+  memo,
+  useContext,
+  useEffect,
+  useState,
+  ReactElement,
+} from "react";
+import { Link, useLocation } from "react-router-dom";
+import styles from "./Menu.module.scss";
 
 const { Text } = Typography;
 
 const Menu: React.FC<{}> = React.memo((props) => {
   const location = useLocation();
-  const [link, setLink] = useState('/');
+  const [link, setLink] = useState("/");
   const { theme, setTheme } = useContext(ThemeContext);
 
   useEffect(() => {
     setLink(location.pathname);
   }, []);
 
-  const renderLink = (to: string, title: string, onClick: any) => {
+  const renderLink = (
+    to: string,
+    title: string,
+    onClick: any,
+    icon: ReactElement
+  ) => {
     return (
-      <Link to={to} onClick={() => onClick(to)} className={styles.menu_item  + (link === to ? ` ${styles.active}` : '')}>
-        <Logo style={{ width: 30, height: 30 }} />
+      <Link
+        to={to}
+        onClick={() => onClick(to)}
+        className={styles.menu_item + (link === to ? ` ${styles.active}` : "")}
+      >
+        {icon}
+        {/* <Logo style={{ width: 30, height: 30 }} /> */}
         <Text className={styles.menu_item_text}>{title}</Text>
       </Link>
     );
@@ -27,27 +46,58 @@ const Menu: React.FC<{}> = React.memo((props) => {
 
   return (
     <div className={styles.menu}>
-      <Link to={'/'} onClick={() => setLink('/')} className={styles.logo}>
-          <Logo style={{ width: 40, height: 40 }} />
-          <Text className={styles.logo_text}>OraiBridge</Text>
+      <Link to={"/"} onClick={() => setLink("/")} className={styles.logo}>
+        {<Logo style={{ width: 40, height: 40 }} />}
+        <Text className={styles.logo_text}>OraiBridge</Text>
       </Link>
       <div className={styles.menu_items}>
-          {renderLink('/swap', 'Swap', setLink)}
-          {renderLink('/pools', 'Pools', setLink)}
-          {renderLink('/balance', 'Balance', setLink)}
+        {renderLink(
+          "/swap",
+          "Swap",
+          setLink,
+          <Swap style={{ width: 30, height: 30 }} />
+        )}
+        {renderLink(
+          "/pools",
+          "Pools",
+          setLink,
+          <Pools style={{ width: 30, height: 30 }} />
+        )}
+        {renderLink(
+          "/balance",
+          "Balance",
+          setLink,
+          <Wallet style={{ width: 30, height: 30 }} />
+        )}
       </div>
 
       <div className={styles.menu_themes}>
-        <Button className={styles.menu_theme + (theme == Themes.dark ? ` ${styles.active}` : "")} onClick={() => {setTheme(Themes.dark)}}>
+        <Button
+          className={
+            styles.menu_theme +
+            (theme == Themes.dark ? ` ${styles.active}` : "")
+          }
+          onClick={() => {
+            setTheme(Themes.dark);
+          }}
+        >
           <Logo style={{ width: 20, height: 20 }} />
           <Text className={styles.menu_theme_text}>Dark</Text>
         </Button>
-        <Button className={styles.menu_theme + (theme == Themes.light ? ` ${styles.active}` : "")} onClick={() => {setTheme(Themes.light)}}>
+        <Button
+          className={
+            styles.menu_theme +
+            (theme == Themes.light ? ` ${styles.active}` : "")
+          }
+          onClick={() => {
+            setTheme(Themes.light);
+          }}
+        >
           <Logo style={{ width: 20, height: 20 }} />
           <Text className={styles.menu_theme_text}>Light</Text>
         </Button>
       </div>
-      
+
       <div className={styles.menu_footer}>© 2022 Powered by Oraichain</div>
     </div>
   );
