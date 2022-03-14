504f73acf3cc61a95ec90bf625d034493bc65bcf
ducphamle2 <ducphamle212@gmail.com>
2022-03-14 23:36:13 +0700

    uncomment testnet config

--- src/constants/bridgeTokens.ts
+++ src/constants/bridgeTokens.ts
@@ -23,128 +23,145 @@ export type TokenItemType = {
 };
 
 const tokensMap: { [key: string]: [TokenItemType[], TokenItemType[]] } = {
-  // [NetworkKey.TESTNET]: [
-  //   [
-  //     {
-  //       name: 'ORAI',
-  //       org: 'Ethereum',
-  //       denom: '0x7e2a35c746f2f7c240b664f1da4dd100141ae71f',
-  //       coingeckoId: 'oraichain-token',
-  //       decimals: 6,
-  //       chainId: 'Oraichain-testnet',
-  //       rpc: 'https://testnet.rpc.orai.io',
-  //       lcd: 'https://testnet.lcd.orai.io',
-  //       cosmosBased: false,
-  //       Icon: ORAI
-  //     },
-  //     {
-  //       name: 'ATOM',
-  //       org: 'Cosmos Hub',
-  //       coingeckoId: 'atom',
-  //       denom: 'atom',
-  //       decimals: 6,
-  //       chainId: 'cosmoshub-4',
-  //       rpc: 'https://rpc-cosmoshub.blockapsis.com',
-  //       lcd: 'https://lcd-cosmoshub.blockapsis.com',
-  //       cosmosBased: true,
-  //       Icon: ATOMCOSMOS
-  //     },
-  //     {
-  //       name: 'OSMO',
-  //       org: 'Osmosis',
-  //       denom: 'osmosis',
-  //       chainId: 'osmosis-1',
-  //       rpc: 'https://rpc-osmosis.blockapsis.com',
-  //       lcd: 'https://lcd-osmosis.blockapsis.com',
-  //       decimals: 6,
-  //       coingeckoId: 'osmosis',
-  //       cosmosBased: true,
-  //       Icon: OSMO
-  //     },
-  //     {
-  //       name: 'ETH',
-  //       org: 'Ethereum',
-  //       coingeckoId: 'ethereum',
-  //       denom: 'ethereum',
-  //       decimals: 18,
-  //       chainId: 'ethereum',
-  //       rpc: 'http://125.212.192.225:26657',
-  //       cosmosBased: false,
-  //       Icon: ETH
-  //     },
-  //     {
-  //       name: 'BNB',
-  //       org: 'BNB Chain',
-  //       chainId: 'bsc',
-  //       denom: 'bnb',
-  //       rpc: 'https://data-seed-prebsc-1-s1.binance.org:8545',
-  //       decimals: 18,
-  //       coingeckoId: 'bnb',
-  //       cosmosBased: false,
-  //       Icon: BNB
-  //     }
-  //   ],
-  //   [
-  //     {
-  //       name: 'ORAI',
-  //       org: 'Oraichain',
-  //       denom: 'orai',
-  //       coingeckoId: 'oraichain-token',
-  //       decimals: 6,
-  //       chainId: 'Oraichain-testnet',
-  //       rpc: 'https://testnet.rpc.orai.io',
-  //       lcd: 'https://testnet.lcd.orai.io',
-  //       cosmosBased: true,
-  //       Icon: ORAI
-  //     },
-  //     {
-  //       name: 'ATOM',
-  //       org: 'Oraichain',
-  //       coingeckoId: 'atom',
-  //       denom: 'ibc/atom',
-  //       decimals: 6,
-  //       chainId: 'Oraichain-testnet',
-  //       rpc: 'https://testnet.rpc.orai.io',
-  //       lcd: 'https://testnet.lcd.orai.io',
-  //       cosmosBased: true,
-  //       Icon: ATOMCOSMOS
-  //     },
-  //     {
-  //       name: 'OSMO',
-  //       org: 'Oraichain',
-  //       denom: 'ibc/osmosis',
-  //       chainId: 'Oraichain-testnet',
-  //       rpc: 'https://testnet.rpc.orai.io',
-  //       lcd: 'https://testnet.lcd.orai.io',
-  //       decimals: 6,
-  //       coingeckoId: 'osmosis',
-  //       cosmosBased: true,
-  //       Icon: OSMO
-  //     },
-  //     {
-  //       name: 'ETH',
-  //       org: 'Oraichain',
-  //       coingeckoId: 'ethereum',
-  //       denom: 'ibc/ethereum',
-  //       decimals: 18,
-  //       chainId: 'Oraichain-testnet',
-  //       rpc: 'http://125.212.192.225:26657',
-  //       cosmosBased: false,
-  //       Icon: ETH
-  //     },
-  //     {
-  //       name: 'BNB',
-  //       org: 'Oraichain',
-  //       chainId: 'Oraichain-testnet',
-  //       denom: 'bnb',
-  //       rpc: 'https://data-seed-prebsc-1-s1.binance.org:8545',
-  //       decimals: 18,
-  //       coingeckoId: 'bnb',
-  //       cosmosBased: false,
-  //       Icon: BNB
-  //     }
-  //   ]
-  // ],
+  [NetworkKey.TESTNET]: [
+    [
+      {
+        name: 'ORAI',
+        org: 'Ethereum',
+        denom: '0x7e2a35c746f2f7c240b664f1da4dd100141ae71f',
+        contractAddress: "ORAI",
+        coingeckoId: 'oraichain-token',
+        decimals: 6,
+        chainId: 'Oraichain-testnet',
+        rpc: 'https://testnet.rpc.orai.io',
+        lcd: 'https://testnet.lcd.orai.io',
+        cosmosBased: false,
+        Icon: ORAI
+      },
+      {
+        name: 'ATOM',
+        org: 'Cosmos Hub',
+        coingeckoId: 'cosmos',
+        denom: 'atom',
+        decimals: 6,
+        chainId: 'cosmoshub-4',
+        rpc: 'https://rpc-cosmoshub.blockapsis.com',
+        lcd: 'https://lcd-cosmoshub.blockapsis.com',
+        cosmosBased: false,
+        Icon: ATOMCOSMOS
+      },
+      {
+        name: 'OSMO',
+        org: 'Osmosis',
+        denom: 'osmosis',
+        chainId: 'osmosis-1',
+        rpc: 'https://rpc-osmosis.blockapsis.com',
+        lcd: 'https://lcd-osmosis.blockapsis.com',
+        decimals: 6,
+        coingeckoId: 'osmosis',
+        cosmosBased: false,
+        Icon: OSMO
+      },
+      {
+        name: 'ETH',
+        org: 'Ethereum',
+        coingeckoId: 'ethereum',
+        denom: 'ethereum',
+        decimals: 18,
+        chainId: 'ethereum',
+        rpc: 'http://125.212.192.225:26657',
+        cosmosBased: false,
+        Icon: ETH
+      },
+      {
+        name: 'BNB',
+        org: 'BNB Chain',
+        chainId: 'bsc',
+        denom: 'bnb',
+        rpc: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+        decimals: 18,
+        coingeckoId: 'bnb',
+        cosmosBased: false,
+        Icon: BNB
+      }
+    ],
+    [
+      {
+        name: 'ORAI',
+        org: 'Oraichain',
+        denom: 'orai',
+        contractAddress: "ORAI",
+        coingeckoId: 'oraichain-token',
+        decimals: 6,
+        chainId: 'Oraichain-testnet',
+        rpc: 'https://testnet.rpc.orai.io',
+        lcd: 'https://testnet.lcd.orai.io',
+        cosmosBased: true,
+        Icon: ORAI,
+        logo: "oraichain.svg"
+      },
+      {
+        name: 'ATOM',
+        org: 'Oraichain',
+        coingeckoId: 'cosmos',
+        denom: 'ibc/atom',
+        decimals: 6,
+        chainId: 'Oraichain-testnet',
+        rpc: 'https://testnet.rpc.orai.io',
+        lcd: 'https://testnet.lcd.orai.io',
+        cosmosBased: false,
+        Icon: ATOMCOSMOS
+      },
+      {
+        name: 'OSMO',
+        org: 'Oraichain',
+        denom: 'ibc/osmosis',
+        chainId: 'Oraichain-testnet',
+        rpc: 'https://testnet.rpc.orai.io',
+        lcd: 'https://testnet.lcd.orai.io',
+        decimals: 6,
+        coingeckoId: 'osmosis',
+        cosmosBased: false,
+        Icon: OSMO
+      },
+      {
+        name: 'ETH',
+        org: 'Oraichain',
+        coingeckoId: 'ethereum',
+        denom: 'ibc/ethereum',
+        decimals: 18,
+        chainId: 'Oraichain-testnet',
+        rpc: 'http://125.212.192.225:26657',
+        cosmosBased: false,
+        Icon: ETH
+      },
+      {
+        name: 'BNB',
+        org: 'Oraichain',
+        chainId: 'Oraichain-testnet',
+        denom: 'bnb',
+        rpc: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+        decimals: 18,
+        coingeckoId: 'bnb',
+        cosmosBased: false,
+        Icon: BNB
+      },
+      {
+        name: 'AIRI',
+        org: 'Oraichain',
+        coingeckoId: 'airight',
+        denom: '',
+        contractAddress: 'orai1gwe4q8gme54wdk0gcrtsh4ykwvd7l9n3dxxas2',
+        decimals: 6,
+        chainId: 'Oraichain',
+        rpc: 'https://testnet-rpc.orai.io',
+        lcd: 'https://testnet-lcd.orai.io',
+        cosmosBased: true,
+        Icon: AIRI,
+        logo: 'airi.svg'
+      },
+    ]
+  ],
   [NetworkKey.MAINNET]: [
     [
       {
