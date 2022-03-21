a19d3834e5a507f700993fd6d4250d583b513c37
Pham Tu <tubackkhoa@gmail.com>
2022-03-21 16:11:04 +0700

    mobile css

--- src/libs/keplr.ts
+++ src/libs/keplr.ts
@@ -1,4 +1,5 @@
 import { ChainInfo } from '@keplr-wallet/types';
+import { isMobile } from '@walletconnect/browser-utils';
 import { network } from 'constants/networks';
 import { embedChainInfos } from 'networks';
 
@@ -25,7 +26,80 @@ export default class Keplr {
     await window.keplr.enable(chainInfo.chainId);
   };
 
+  // private getKeplrMobile: Promise<Keplr | undefined> {
+
+  //     if (!this.walletConnector) {
+  //       this.walletConnector = new WalletConnect({
+  //         bridge: 'https://bridge.walletconnect.org',
+  //         signingMethods: [],
+  //         qrcodeModal: new WalletConnectQRCodeModalV1Renderer()
+  //       });
+  //       // XXX: I don't know why they designed that the client meta options in the constructor should be always ingored...
+  //       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //       // @ts-ignore
+  //       this.walletConnector._clientMeta = {
+  //         name: 'Osmosis',
+  //         description: 'Osmosis is the first IBC-native Cosmos interchain AMM',
+  //         url: 'https://app.osmosis.zone',
+  //         icons: [
+  //           window.location.origin + '/public/assets/osmosis-wallet-connect.png'
+  //         ]
+  //       };
+
+  //       this.walletConnector!.on(
+  //         'disconnect',
+  //         this.onWalletConnectDisconnected
+  //       );
+  //     }
+
+  //     if (!this.walletConnector.connected) {
+  //       return new Promise<Keplr | undefined>((resolve, reject) => {
+  //         this.walletConnector!.connect()
+  //           .then(() => {
+  //             localStorage?.removeItem(KeyConnectingWalletType);
+  //             localStorage?.setItem(
+  //               KeyAutoConnectingWalletType,
+  //               'wallet-connect'
+  //             );
+  //             this.autoConnectingWalletType = 'wallet-connect';
+
+  //             resolve(
+  //               new KeplrWalletConnectV1(this.walletConnector!, {
+  //                 sendTx,
+  //                 onBeforeSendRequest: this.onBeforeSendRequest
+  //               })
+  //             );
+  //           })
+  //           .catch((e) => {
+  //             console.log(e);
+  //             // XXX: Due to the limitation of cureent account store implementation.
+  //             //      We shouldn't throw an error (reject) on the `getKeplr()` method.
+  //             //      So return the `undefined` temporarily.
+  //             //      In this case, the wallet will be considered as `NotExist`
+  //             resolve(undefined);
+  //           });
+  //       });
+  //     } else {
+  //       localStorage?.removeItem(KeyConnectingWalletType);
+  //       localStorage?.setItem(KeyAutoConnectingWalletType, 'wallet-connect');
+  //       this.autoConnectingWalletType = 'wallet-connect';
+
+  //       return Promise.resolve(
+  //         new KeplrWalletConnectV1(this.walletConnector, {
+  //           sendTx,
+  //           onBeforeSendRequest: this.onBeforeSendRequest
+  //         })
+  //       );
+  //     }
+
+  // }
+
   async getKeplr(): Promise<keplrType | undefined> {
+    if (isMobile()) {
+      console.log('isMobile');
+      return;
+    }
+
     if (window.keplr) {
       return window.keplr;
     }
