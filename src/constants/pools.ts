7b60d4cb4868ee8543e1a4831a2636684901c13b
ducphamle2 <ducphamle212@gmail.com>
2022-03-29 15:04:34 +0700

    update contract addr for pools

--- src/constants/pools.ts
+++ src/constants/pools.ts
@@ -32,28 +32,28 @@ const pairs: { [networkKey: string]: [{ [key: string]: Pair }] } = {
         asset_denoms: ['orai', 'airi']
       },
       [PairKey.ORAI_ATOM]: {
-        contract_addr: 'orai12j04ae0ql3jmmj20j97ve7q9u6guwxkv4wm8g3',
+        contract_addr: 'orai1jf74ry4m0jcy9emsaudkhe7vte9l8qy8enakvs',
         asset_denoms: [
           'orai',
           'ibc/A2E2EEC9057A4A1C2C0A6A4C78B0239118DF5F278830F50B4A6BDD7A66506B78'
         ]
       },
       [PairKey.ORAI_UST]: {
-        contract_addr: 'orai13us9ewmhyxa3ntl7vyg0gewvwddudlnkuzd8pk',
+        contract_addr: 'orai14lpk5gnmj688f957xvd6c42w9dygl9c2dlxjzw',
         asset_denoms: [
           'orai',
           'ibc/9E4F68298EE0A201969E583100E5F9FAD145BAA900C04ED3B6B302D834D8E3C4'
         ]
       },
       [PairKey.ORAI_LUNA]: {
-        contract_addr: 'orai1ysnprvvmscxj6ukvdm5wtxvu29f9srwtjcrut4',
+        contract_addr: 'orai1d2h9phsu6rau87d2l8adlxfhrdun2wrgdhsqxt',
         asset_denoms: [
           'orai',
           'ibc/BA44E90EAFEA8F39D87A94A4A61C9FFED5887C2730DFBA668C197BA331372859'
         ]
       },
       [PairKey.ORAI_OSMO]: {
-        contract_addr: 'orai1g856tx0kxp035d23k7p62clad7uwhz7pyx235f',
+        contract_addr: 'orai1d37artrk4tkhz2qyjmaulc2jzjkx7206tmpfug',
         asset_denoms: [
           'orai',
           'ibc/9C4DCD21B48231D0BC2AC3D1B74A864746B37E4292694C93C617324250D002FC'
