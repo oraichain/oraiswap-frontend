caf76fe2f1d781c5fd442c797960c1574aedc1c5
Pham Tu <tubackkhoa@gmail.com>
2022-06-08 11:03:37 +0700

    polyfill browser storage

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,10 +1,17 @@
-import { BSC_RPC } from 'config/constants';
+import { BSC_RPC, ORAI } from 'config/constants';
 import { BSC_CHAIN_ID } from 'config/constants';
 import WalletConnectProvider from '@walletconnect/ethereum-provider';
+import { isMobile } from '@walletconnect/browser-utils';
+
+// polyfill for keplr extension
+if (!window.browser || !window.browser.storage) {
+  const { LocalKVStore } = require('@keplr-wallet/common/build/kv-store/local');
+  window.browser = { storage: { local: new LocalKVStore(ORAI) } };
+}
 
 export const initEthereum = async () => {
   // support only https
-  if (!window.ethereum && window.location.protocol === 'https:') {
+  if (isMobile() && !window.ethereum && window.location.protocol === 'https:') {
     const chainId = parseInt(BSC_CHAIN_ID, 16);
     const provider = new WalletConnectProvider({
       chainId,
