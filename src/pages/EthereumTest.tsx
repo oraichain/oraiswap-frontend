192e7a195d43ca8a47b50d702b04d9c92907acdf
sonlha <lhas1307@gmail.com>
2022-06-17 16:21:37 +0700

    test ethereum

--- src/pages/EthereumTest.tsx
+++ src/pages/EthereumTest.tsx
@@ -15,34 +15,43 @@ const EthereumTest: FunctionComponent = () => {
     try {
       setClaimLoading(true);
 
-      console.log("window ethereum: ", parseInt('100', 16))
-      window.ReactNativeWebView?.postMessage(String(window.ethereum))
+      console.log('window ethereum: ', parseInt('100', 16));
+      window.ReactNativeWebView?.postMessage(String(window.ethereum));
 
       const web3 = new Web3(window.ethereum);
 
-      // const nonce = await window.ethereum.request({ method: 'eth_getTransactionCount', params: ['0x3C5C6b570C1DA469E8B24A2E8Ed33c278bDA3222', 'latest'], rpc: 'https://endpoint1.kawaii.global' });
-      // alert(`nonce: ${nonce}`)
+      const nonce = await window.ethereum.request({
+        method: 'eth_getTransactionCount',
+        params: ['0x3C5C6b570C1DA469E8B24A2E8Ed33c278bDA3222', 'latest'],
+        rpc: 'https://endpoint1.kawaii.global',
+      });
+      alert(`nonce: ${nonce}`);
 
-      const accounts = await window.ethereum.request({ method: 'eth_accounts', params: [], chainId: 'kawaii_6886-1' });
-      alert(`accounts: ${accounts}`)
-
-
-      // const result = await window.ethereum.request({
-      //   method: 'eth_sendTransaction',
-      //   params: [{
-      //     from: '0x3C5C6b570C1DA469E8B24A2E8Ed33c278bDA3222',
-      //     to: '0x3C5C6b570C1DA469E8B24A2E8Ed33c278bDA3222',
-      //     value: web3.utils.toHex(1),
-      //   }],
+      // const accounts = await window.ethereum.request({
+      //   method: 'eth_accounts',
+      //   params: [],
       //   chainId: 'kawaii_6886-1',
-      //   signer: '0x3C5C6b570C1DA469E8B24A2E8Ed33c278bDA3222'
-      // })
-
-      // console.log("result: ", result);
-      // displayToast(TToastType.TX_SUCCESSFUL, {
-      //   customLink: `https://scan.kawaii.global/tx/${result}`,
       // });
+      // alert(`accounts: ${accounts}`);
 
+      const result = await window.ethereum.request({
+        method: 'eth_sendTransaction',
+        params: [
+          {
+            from: '0x3C5C6b570C1DA469E8B24A2E8Ed33c278bDA3222',
+            to: '0x3C5C6b570C1DA469E8B24A2E8Ed33c278bDA3222',
+            value: web3.utils.toHex(1),
+          },
+        ],
+        chainId: 'kawaii_6886-1',
+        signer: '0x3C5C6b570C1DA469E8B24A2E8Ed33c278bDA3222',
+      });
+
+      console.log('result: ', result);
+
+      displayToast(TToastType.TX_SUCCESSFUL, {
+        customLink: `https://scan.kawaii.global/tx/${result}`,
+      });
     } catch (error: any) {
       console.log('error message handle claim: ', error);
       return displayToast(TToastType.TX_FAILED, {
@@ -70,8 +79,7 @@ const EthereumTest: FunctionComponent = () => {
         <button
           style={{
             background: '#612fca',
-            boxShadow:
-              'inset 0px -2px 0px #4b2993, inset 1px 2px 0px #804af2',
+            boxShadow: 'inset 0px -2px 0px #4b2993, inset 1px 2px 0px #804af2',
             padding: 16,
             borderRadius: 10,
             color: '#ffffff',
@@ -93,11 +101,15 @@ const EthereumTest: FunctionComponent = () => {
               alignItems: 'center',
             }}
           >
-            {claimLoading && <div style={{ marginRight: 5 }}><Loader width={25} height={25} /></div>}
+            {claimLoading && (
+              <div style={{ marginRight: 5 }}>
+                <Loader width={25} height={25} />
+              </div>
+            )}
           </div>
-        </button >
+        </button>
       </div>
-    </Content >
+    </Content>
   );
 };
 
