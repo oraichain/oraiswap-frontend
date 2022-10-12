2d6a9fef2aadad541d0cd914d92faff71d0afbdf
ducphamle2 <ducphamle212@gmail.com>
2022-10-12 08:52:04 -0700

    update: fix swap bug & change back to old milky token

--- src/config/pools.ts
+++ src/config/pools.ts
@@ -48,6 +48,13 @@ const pairsMap: Record<NetworkKey, Pair[]> = {
         ORAI,
         process.env.REACT_APP_OSMOSIS_ORAICHAIN_DENOM
       ]
+    },
+    {
+      contract_addr: 'orai1hr2l03ep6p9lwdkuqu5253fgpzc40xcpwymjfc',
+      asset_denoms: [
+        'milky',
+        STABLE_DENOM,
+      ]
     }
   ],
 };
