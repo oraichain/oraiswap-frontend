5fc7cb11c9c230dde9409c127e4c7eb360c2e250
Hau Nguyen Van <nguyenvanhauit1997@gmail.com>
2023-02-21 15:50:09 +0700

    update redux

--- src/store/configure.tsx
+++ src/store/configure.tsx
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../reducer/counter';
+import amountReducer from '../reducer/amount';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    amount: amountReducer,
   },
 })
 
