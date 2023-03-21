5233520b04bfe6184662713bd29cba0d46d78e9a
trungbach <trunga2k29@gmail.com>
2023-03-21 10:56:25 +0700

    call balances via rpc

--- src/pages/BalanceNew/StuckOraib/useGetOraiBridgeBalances.tsx
+++ src/pages/BalanceNew/StuckOraib/useGetOraiBridgeBalances.tsx
@@ -1,18 +1,16 @@
+import { Coin, StargateClient } from "@cosmjs/stargate";
 import { TokenItemType, tokens } from "config/bridgeTokens";
 import {
-    ORAI_BRIDGE_CHAIN_ID,
-    ORAI_BRIDGE_LCD
+    ORAI_BRIDGE_CHAIN_ID, ORAI_BRIDGE_RPC
 } from 'config/constants';
 import { toDisplay } from 'libs/utils';
 import { useEffect, useState } from 'react';
-import token from "reducer/token";
-import axios from 'rest/request';
-import { Balance, BalancesOraiBridge } from '../type';
 
 export type RemainingOraibTokenItem = TokenItemType & { amount: string };
 const ORAIB_DECIMALS = 18;
 export default function useGetOraiBridgeBalances(moveOraib2OraiLoading: boolean) {
     const [remainingOraib, setRemainingOraib] = useState<RemainingOraibTokenItem[]>([])
+    const [otherChainTokens] = tokens
 
     const getBalanceOraibridge = async () => {
         try {
@@ -21,14 +19,14 @@ export default function useGetOraiBridgeBalances(moveOraib2OraiLoading: boolean)
                 setRemainingOraib([]);
                 return;
             }
-            const { data }: { data: BalancesOraiBridge } = await axios.get(`${ORAI_BRIDGE_LCD}/cosmos/bank/v1beta1/balances/${oraiBridgeAddress}`);
-            const [otherChainTokens] = tokens
-            const remainingOraib = data.balances.filter((item: Balance) => toDisplay(item.amount, ORAIB_DECIMALS) > 0)
-                .map((item: Balance) => {
+            const client = await StargateClient.connect(ORAI_BRIDGE_RPC);
+            const data: readonly Coin[] = await client.getAllBalances(oraiBridgeAddress);
+            const remainingOraib = data.filter((item: Coin) => toDisplay(item.amount, ORAIB_DECIMALS) > 0)
+                .map((item: Coin) => {
                     const findedOraib = otherChainTokens.find((token: TokenItemType) => token.denom.toLowerCase() === item.denom.toLowerCase())
                     return {
                         ...findedOraib,
-                        amount: item.amount 
+                        amount: item.amount
                     } as RemainingOraibTokenItem
                 })
 
@@ -41,6 +39,7 @@ export default function useGetOraiBridgeBalances(moveOraib2OraiLoading: boolean)
     // refetch balance
     useEffect(() => {
         !moveOraib2OraiLoading && getBalanceOraibridge();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [moveOraib2OraiLoading]);
 
     return { remainingOraib }
