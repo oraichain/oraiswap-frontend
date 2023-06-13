cc331e96d1ca853229c74583ca812f3c2bc0aecd
Hau Nguyen Van <nguyenvanhauit1997@gmail.com>
2023-06-13 15:45:11 +0700

    add testcase frontier

--- src/tests/frontier/token.spec.ts
+++ src/tests/frontier/token.spec.ts
@@ -1,8 +1,4 @@
-// import { addPairAndLpToken, msgsTextProposal, deployCw20Token, getPairAndLpAddress } from 'libs/frontier/token';
-import { toAmount } from 'libs/utils';
-import { deployOraiDexContracts } from 'tests/listing-simulate';
-import { network } from 'config/networks';
-import { CODE_ID_CW20 } from 'config/constants';
+import { ORAI } from 'config/constants';
 import { generateMsgFrontierAddToken, getInfoLiquidityPool } from 'pages/Pools/helpers';
 
 describe('frontier-cw20-token', () => {
@@ -13,19 +9,26 @@ describe('frontier-cw20-token', () => {
     project: null
   };
   const tokenName = 'BabyMario';
-  const name = '';
+  const name = 'test';
+  const label = 'label';
   const isNewReward = [
     {
       name: 'orai',
       denom: 'orai',
       value: BigInt(1e6),
       contract_addr: ''
+    },
+    {
+      name: 'milky',
+      denom: 'milky',
+      value: BigInt(1e6),
+      contract_addr: process.env.REACT_APP_MILKY_CONTRACT
     }
   ];
 
   const mint = {
     minter: 'orai12zyu8w93h0q2lcnt50g3fn0w3yqnhy4fvawaqz',
-    cap: '100000000000000'
+    cap: BigInt(0).toString()
   };
 
   const initBalances = [
@@ -35,88 +38,87 @@ describe('frontier-cw20-token', () => {
     }
   ];
 
-  it('msgs-list-token', async () => {
-    const initialBalances = initBalances.map((e) => ({ ...e, amount: e?.amount.toString() }));
-    const liquidityPoolRewardAssets = isNewReward.map((isReward) => {
+  let liquidityPoolRewardAssets = [];
+  let initialBalances = [];
+
+  it('get-info-native-token-liquidity-pool', async () => {
+    const nativeTokenInfo = getInfoLiquidityPool({ denom: ORAI, contract_addr: '' });
+    expect(typeof nativeTokenInfo === 'object').toBe(true);
+    expect(nativeTokenInfo).toHaveProperty('native_token');
+    expect(nativeTokenInfo).toEqual({
+      native_token: {
+        denom: ORAI
+      }
+    });
+  });
+
+  it('get-info-cw20-token-liquidity-pool', async () => {
+    const cw20TokenInfo = getInfoLiquidityPool({
+      denom: 'milky',
+      contract_addr: process.env.REACT_APP_AIRI_CONTRACT
+    });
+    expect(typeof cw20TokenInfo === 'object').toBe(true);
+    expect(cw20TokenInfo).toHaveProperty('token');
+    expect(cw20TokenInfo).toEqual({
+      token: {
+        contract_addr: process.env.REACT_APP_AIRI_CONTRACT
+      }
+    });
+  });
+
+  it('get-init-balances', async () => {
+    initialBalances = initBalances.map((e) => ({ ...e, amount: e?.amount.toString() }));
+    expect(Array.isArray(initialBalances)).toBe(true);
+    for (const info of initialBalances) {
+      expect(info.amount).toBe(initBalances[0]?.amount.toString());
+    }
+  });
+
+  it('get-liquidity-pool-reward-assets', async () => {
+    liquidityPoolRewardAssets = isNewReward.map((isReward) => {
       return {
         amount: isReward?.value.toString(),
         info: getInfoLiquidityPool(isReward)
       };
     });
+    expect(Array.isArray(liquidityPoolRewardAssets)).toBe(true);
+    for (const liquidity of liquidityPoolRewardAssets) {
+      expect(liquidity.amount).toBe(isNewReward[0]?.value.toString());
+      if (liquidity.info.native_token) {
+        expect(liquidity.info).toEqual({
+          native_token: {
+            denom: ORAI
+          }
+        });
+      }
+      if (liquidity.info.token) {
+        expect(liquidity.info).toEqual({
+          token: {
+            contract_addr: process.env.REACT_APP_MILKY_CONTRACT
+          }
+        });
+      }
+    }
+  });
 
+  it('msgs-list-token', async () => {
     const msgs = await generateMsgFrontierAddToken({
       marketing,
       symbol: tokenName,
       liquidityPoolRewardAssets,
       name,
       initialBalances,
-      mint
+      mint,
+      label
     });
-
-    expect(msgs && typeof msgs === 'object').toBe(true);
+    expect(typeof msgs === 'object').toBe(true);
     expect(msgs).toHaveProperty('marketing');
     expect(msgs).toHaveProperty('symbol');
+    expect(msgs).toHaveProperty('label');
     expect(msgs).toHaveProperty('mint');
     expect(msgs).toHaveProperty('name');
     expect(msgs).toHaveProperty('initialBalances');
     expect(Array.isArray(msgs.initialBalances)).toBe(true);
     expect(Array.isArray(msgs.liquidityPoolRewardAssets)).toBe(true);
   });
-
-  // beforeAll(async () => {
-  //   const { factory, tokenCodeId } = await deployOraiDexContracts();
-  //   factoryContract = factory;
-  //   codeId = tokenCodeId;
-  // });
-
-  // it('deploy-cw20-token', async () => {
-  //   cw20ContractAddress = await deployCw20Token({
-  //     tokenSymbol,
-  //     client,
-  //     address: senderAddress,
-  //     codeId
-  //   });
-  //   expect(typeof cw20ContractAddress).toBe('string');
-  // });
-
-  // it('add-pair-and-lp-token', async () => {
-  //   const res = await addPairAndLpToken({
-  //     cw20ContractAddress,
-  //     factory: factoryContract,
-  //     client,
-  //     address: senderAddress
-  //   });
-
-  //   if (res.logs?.length) {
-  //     const { pairAddress, lpAddress } = getPairAndLpAddress(res);
-  //     pairAddressSimulate = pairAddress;
-  //     lpAddressSimulate = lpAddress;
-  //   } else {
-  //     const wasmAttributes = res?.events.find((event) => {
-  //       return event.type === 'wasm' && event.attributes.find((attr) => attr.key === 'liquidity_token_address');
-  //     })?.attributes;
-  //     pairAddressSimulate = wasmAttributes?.find((attr) => attr.key === 'pair_contract_address')?.value;
-  //     lpAddressSimulate = wasmAttributes?.find((attr) => attr.key === 'liquidity_token_address')?.value;
-  //   }
-  //   expect(typeof pairAddressSimulate).toBe('string');
-  //   expect(typeof lpAddressSimulate).toBe('string');
-  // });
-
-  // it('msgs-create-text-proposal', async () => {
-  //   const msgs = await msgsTextProposal({
-  //     cw20ContractAddress,
-  //     lpAddress: lpAddressSimulate,
-  //     address: senderAddress,
-  //     rewardPerSecondOrai,
-  //     rewardPerSecondOraiX
-  //   });
-
-  //   expect(msgs && typeof msgs === 'object').toBe(true);
-  //   expect(msgs).toHaveProperty('typeUrl');
-  //   expect(msgs).toHaveProperty('value');
-  //   expect(typeof msgs.value === 'object').toBe(true);
-  //   expect(msgs.value).toHaveProperty('content');
-  //   expect(msgs.value).toHaveProperty('proposer');
-  //   expect(msgs.value).toHaveProperty('initialDeposit');
-  // });
 });
