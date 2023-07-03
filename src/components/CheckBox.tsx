676a48834f53af7dbc9fc9a44a4ccee54df0aedc
Hau Nguyen Van <nguyenvanhauit1997@gmail.com>
2023-07-03 16:07:04 +0700

    add pair asset frontier

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -4,10 +4,11 @@ import styles from './CheckBox.module.scss';
 
 const CheckBox: FC<{
   className?: string;
+  radioBox?: boolean;
   label?: string;
   checked: boolean;
   onCheck: Function;
-}> = ({ className, label, checked = false, onCheck }) => {
+}> = ({ className, label, checked = false, onCheck, radioBox }) => {
   return (
     <label className={classNames(styles.container, className)}>
       {label}
@@ -18,8 +19,8 @@ const CheckBox: FC<{
           onCheck(e.target.checked);
         }}
       />
-      <span className={classNames(styles.checkmark)}></span>
-    </label>
+      <span className={classNames(styles.checkmark, radioBox && styles.radioBox)}></span>
+    </label >
   );
 };
 export default CheckBox;
