02e19c8ab10ba789f692308825e670c68565d885
trungbach <trunga2k29@gmail.com>
2023-08-07 16:28:33 +0700

    fix display chart when switch symbol

--- src/reducer/tradingSlice.ts
+++ src/reducer/tradingSlice.ts
@@ -3,12 +3,10 @@ import { createSlice } from '@reduxjs/toolkit';
 import { RootState } from 'store/configure';
 import { PairToken, TradingState } from './type';
 import { pairsChart } from 'components/TVChartContainer/config';
-import { Bar } from 'components/TVChartContainer/helpers/types';
 
 const initialState: TradingState = {
   listToken: [],
   currentToken: pairsChart.find((pair) => pair.symbol === 'ORAI/USDT'),
-  chartLoading: false,
   chartDataLength: -1
 };
 
@@ -22,9 +20,6 @@ const tradingSlice = createSlice({
     setCurrentToken: (state, action: PayloadAction<PairToken>) => {
       state.currentToken = action.payload;
     },
-    setChartLoading: (state, action: PayloadAction<boolean>) => {
-      state.chartLoading = action.payload;
-    },
     setChartDataLength: (state, action: PayloadAction<number>) => {
       state.chartDataLength = action.payload;
     }
@@ -32,10 +27,9 @@ const tradingSlice = createSlice({
 });
 
 // Action creators are generated for each case reducer function
-export const { setCurrentToken, setListToken, setChartLoading, setChartDataLength } = tradingSlice.actions;
+export const { setCurrentToken, setListToken, setChartDataLength } = tradingSlice.actions;
 
 export const selectCurrentToken = (state: RootState): PairToken => state.trading.currentToken;
-export const selectChartLoading = (state: RootState): boolean => state.trading.chartLoading;
 export const selectListToken = (state: RootState): PairToken[] => state.trading.listToken;
 export const selectChartDataLength = (state: RootState): number => state.trading.chartDataLength;
 
