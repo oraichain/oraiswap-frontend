90f94df0b97046a092f00e94dd8a7391852a3276
trungbach <trunga2k29@gmail.com>
2023-09-18 18:39:26 +0700

    add table pools

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,50 @@
+import { ReactNode } from 'react';
+import styles from './Table.module.scss';
+
+
+export type TableHeaderProps<T extends object> = Record<string, {
+    name: string;
+    accessor: (data: T) => ReactNode | string | undefined;
+    width: string;
+    align: 'left' | 'right' | 'center';
+}>
+export type TableProps<T extends object> = {
+    headers: TableHeaderProps<T>
+    data: T[]
+}
+
+const Table = <T extends object>({ headers, data }: TableProps<T>) => {
+    return (<table className={styles.table}>
+        <thead className={styles.tableHeader}>
+            <tr>
+                {Object.keys(headers).map((key, index) => {
+                    return (
+                        <th scope="col" key={index} style={{ width: headers[key].width, textAlign: headers[key].align }}>
+                            {headers[key].name}
+                        </th>
+                    );
+                })}
+
+            </tr>
+        </thead>
+        <tbody className={styles.tableBody}>
+            {data.map((datum, index) => {
+                return (
+                    <tr key={index}>
+                        {Object.keys(headers).map((key, index) => {
+                            return (
+                                <td key={index} style={{ width: headers[key].width, textAlign: headers[key].align }}>
+                                    <span>{datum[key]}</span>
+                                </td>
+                            )
+
+                        })}
+                    </tr>
+                );
+            })}
+        </tbody>
+    </table>
+    )
+}
+
+export default Table
--- src/pages/Pools/components/ListPool/ListPool.tsx
+++ src/pages/Pools/components/ListPool/ListPool.tsx
@@ -3,11 +3,12 @@ import NoDataLightSvg from 'assets/images/NoDataPoolLight.svg';
 import useConfigReducer from 'hooks/useConfigReducer';
 import useTheme from 'hooks/useTheme';
 import { PairInfoData } from 'pages/Pools/helpers';
-import { memo, useEffect, useState } from 'react';
+import { ReactNode, memo, useEffect, useState } from 'react';
 import { PoolInfoResponse } from 'types/pool';
 import { Filter } from '../Filter';
 import styles from './ListPool.module.scss';
 import { PoolItem } from './PoolItem';
+import Table, { TableHeaderProps } from 'components/Table/Table';
 
 type ListPoolsProps = {
     pairInfos: PairInfoData[];
@@ -23,21 +24,68 @@ export const ListPools = memo<ListPoolsProps>(({ pairInfos, allPoolApr, pools })
         setFilteredPairInfos(pairInfos)
     }, [pairInfos])
 
+    const headers: TableHeaderProps<PoolInfoResponse> = {
+        'symbols': {
+            name: "Pool Name",
+            accessor: (data) => <span>{data.apr}</span>,
+            width: "22%",
+            align: 'left'
+
+        },
+        'apr': {
+            name: "APR",
+            width: "10%",
+            accessor: (data) => <span>{data.apr}</span>,
+            align: 'left'
+        },
+        'my_stake': {
+            name: "My Staked LP",
+            width: "12%",
+            align: 'left',
+            accessor: (data) => <span>{data.apr}</span>,
+        },
+        'earned': {
+            name: "Earned",
+            width: "12%",
+            align: 'center',
+            accessor: (data) => <span>{data.apr}</span>
+        },
+        'fee7Days': {
+            name: "Fee (7D)",
+            width: "10%",
+            align: 'center',
+            accessor: (data) => <span>{data.fee7Days}</span>
+        },
+        'volume24Hour': {
+            name: "Volume (24H)",
+            width: "12%",
+            align: 'right',
+            accessor: (data) => <span>{data.volume24Hour}</span>
+        },
+        'totalLiquidity': {
+            name: "Liquidity",
+            width: "22%",
+            align: 'right',
+            accessor: (data) => <span>{data.totalLiquidity}</span>
+        }
+    }
+
     return (
         <div className={styles.listpools}>
             <Filter setFilteredPairInfos={setFilteredPairInfos} pairInfos={pairInfos} />
 
             <div className={styles.listpools_list}>
                 {filteredPairInfos.length > 0 ? (
-                    filteredPairInfos.map((info) => (
-                        <PoolItem
-                            {...info}
-                            cachedReward={cachedReward}
-                            apr={!!allPoolApr ? allPoolApr[info.pair.contract_addr] : 0}
-                            key={info.pair.contract_addr}
-                            theme={theme}
-                        />
-                    ))
+                    <Table headers={headers} data={pools} />
+                    // filteredPairInfos.map((info) => (
+                    //     <PoolItem
+                    //         {...info}
+                    //         cachedReward={cachedReward}
+                    //         apr={!!allPoolApr ? allPoolApr[info.pair.contract_addr] : 0}
+                    //         key={info.pair.contract_addr}
+                    //         theme={theme}
+                    //     />
+                    // ))
                 ) : (
                     <div className={styles.no_data}>
                         <img src={theme === 'light' ? NoDataLightSvg : NoDataSvg} alt="nodata" />
