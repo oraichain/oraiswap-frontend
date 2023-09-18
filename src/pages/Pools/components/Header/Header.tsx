efb1667cbf03fbdf80abc7c258eaf39c9c390d39
trungbach <trunga2k29@gmail.com>
2023-09-19 00:48:10 +0700

    Style header pool

--- src/pages/Pools/components/Header/Header.tsx
+++ src/pages/Pools/components/Header/Header.tsx
@@ -2,12 +2,12 @@ import TokenBalance from "components/TokenBalance";
 import styles from './Header.module.scss';
 import { FC } from "react";
 
-export const Header: FC<{ theme: string; amount: number; oraiPrice: number }> = ({ amount, oraiPrice, theme }) => {
+export const Header: FC<{ amount: number; oraiPrice: number }> = ({ amount, oraiPrice }) => {
     return (
         <div className={styles.header}>
             <div className={styles.header_data}>
                 <div className={styles.header_data_item}>
-                    <span className={styles.header_data_name}>ORAI Price</span>
+                    <span className={styles.header_data_name}>ORAI Price</span><br />
                     <span className={styles.header_data_value}>
                         <TokenBalance balance={oraiPrice} className={styles.header_data_value} decimalScale={2} />
                     </span>
