58f5b9af8ace0580b0a9a4e127f2fde6d509e2aa
Hau Nguyen Van <nguyenvanhauit1997@gmail.com>
2023-09-28 14:59:11 +0700

    hide frontier pool

--- src/pages/Pools/index.tsx
+++ src/pages/Pools/index.tsx
@@ -193,7 +193,7 @@ const ListPools = memo<{
             <SearchInput theme={theme} placeholder="Search by pools or tokens name" onSearch={filterPairs} />
           </div>
         </div>
-        <div className={styles.listpoolsToken_create}>
+        {/* <div className={styles.listpoolsToken_create}>
           <div
             style={{
               color: '#fff',
@@ -204,7 +204,7 @@ const ListPools = memo<{
           >
             List a new ORAI/CW20 token pool
           </div>
-        </div>
+        </div> */}
       </div>
 
       <div className={styles.listpools_list}>
