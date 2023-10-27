0423bee3e82b1ceacc16bff45ae9ee8778a7aa85
Toan Dang <toan.dq@orai.io>
2023-10-27 10:31:33 +0700

    update icon wallet

--- src/components/ConnectWallet/MyWallet/index.tsx
+++ src/components/ConnectWallet/MyWallet/index.tsx
@@ -39,150 +39,150 @@ const MyWallets: React.FC<{
   toggleShowNetworks,
   wallets
 }) => {
-    const [theme] = useConfigReducer('theme');
+  const [theme] = useConfigReducer('theme');
 
-    const [timeoutCopyId, setTimeoutCopyId] = useState<number>(0);
+  const [timeoutCopyId, setTimeoutCopyId] = useState<number>(0);
 
-    const [copiedAddressCoordinates, setCopiedAddressCoordinates] = useState<{ networkId: string; walletId: number }>({
-      networkId: '',
-      walletId: 0
-    });
+  const [copiedAddressCoordinates, setCopiedAddressCoordinates] = useState<{ networkId: string; walletId: number }>({
+    networkId: '',
+    walletId: 0
+  });
 
-    const getUrlQrCode = async ({ address, icon, name }) => {
-      try {
-        const url = await QRCode.toDataURL(address);
-        setQRUrlInfo({ url, icon, name, address });
-        setIsShowMyWallet(false);
-      } catch (err) {
-        console.error('ERROR getUrlQrCode:', err);
-      }
-    };
+  const getUrlQrCode = async ({ address, icon, name }) => {
+    try {
+      const url = await QRCode.toDataURL(address);
+      setQRUrlInfo({ url, icon, name, address });
+      setIsShowMyWallet(false);
+    } catch (err) {
+      console.error('ERROR getUrlQrCode:', err);
+    }
+  };
 
-    useEffect(() => {
-      if (copiedAddressCoordinates.networkId && copiedAddressCoordinates.walletId) {
-        const TIMEOUT_COPY = 2000;
-        const timeoutId = setTimeout(() => {
-          setCopiedAddressCoordinates({ walletId: 0, networkId: '' });
-        }, TIMEOUT_COPY);
+  useEffect(() => {
+    if (copiedAddressCoordinates.networkId && copiedAddressCoordinates.walletId) {
+      const TIMEOUT_COPY = 2000;
+      const timeoutId = setTimeout(() => {
+        setCopiedAddressCoordinates({ walletId: 0, networkId: '' });
+      }, TIMEOUT_COPY);
 
-        setTimeoutCopyId(Number(timeoutId));
-        return () => clearTimeout(timeoutId);
-      }
-    }, [copiedAddressCoordinates]);
+      setTimeoutCopyId(Number(timeoutId));
+      return () => clearTimeout(timeoutId);
+    }
+  }, [copiedAddressCoordinates]);
 
-    const copyWalletAddress = (e, address: string, walletId: number, networkId: string) => {
-      timeoutCopyId && clearTimeout(timeoutCopyId);
-      if (address) {
-        e.stopPropagation();
-        copy(address);
-        setCopiedAddressCoordinates({ walletId, networkId });
-      }
-    };
+  const copyWalletAddress = (e, address: string, walletId: number, networkId: string) => {
+    timeoutCopyId && clearTimeout(timeoutCopyId);
+    if (address) {
+      e.stopPropagation();
+      copy(address);
+      setCopiedAddressCoordinates({ walletId, networkId });
+    }
+  };
 
-    return (
-      <div className={cx('my_wallets_container', theme)}>
-        <div className={cx('wallet_wrapper')}>
-          {wallets.map((wallet, index) => {
-            return (
-              <div key={index} className={cx('wallet_container')}>
-                <div className={cx('wallet_info')}>
-                  <div className={cx('logo')}>
-                    {wallet.isConnect ? (
-                      <div className={cx('remove')} onClick={() => handleLogoutWallets(wallet.code)}>
-                        <TrashIcon />
-                      </div>
-                    ) : (
-                      <div className={cx('add')} onClick={() => handleLoginWallets(wallet.code)}>
-                        <AddWalletIcon />
-                      </div>
-                    )}
-                    <img src={wallet.icon} alt="wallet icon" />
-                  </div>
-                  <div className={cx('info')}>
-                    <div className={cx('name')}>{wallet.name}</div>
+  return (
+    <div className={cx('my_wallets_container', theme)}>
+      <div className={cx('wallet_wrapper')}>
+        {wallets.map((wallet, index) => {
+          return (
+            <div key={index} className={cx('wallet_container')}>
+              <div className={cx('wallet_info')}>
+                <div className={cx('logo')}>
+                  {wallet.isConnect ? (
+                    <div className={cx('remove')} onClick={() => handleLogoutWallets(wallet.code)}>
+                      <TrashIcon />
+                    </div>
+                  ) : (
+                    <div className={cx('add')} onClick={() => handleLoginWallets(wallet.code)}>
+                      <AddWalletIcon />
+                    </div>
+                  )}
+                  <img src={wallet.icon} alt="wallet icon" />
+                </div>
+                <div className={cx('info')}>
+                  <div className={cx('name')}>{wallet.name}</div>
 
-                    {/* {wallet.isConnect && !!wallet.address && (
+                  {/* {wallet.isConnect && !!wallet.address && (
                     <div>
                       <TokenBalance balance={wallet.totalUsd} className={cx('money')} decimalScale={2} />
                     </div>
                   )} */}
-                  </div>
-                  <div className={cx('control')} onClick={() => toggleShowNetworks(wallet.id)}>
-                    {wallet.isOpen ? <UpArrowIcon /> : <DownArrowIcon />}
-                  </div>
                 </div>
-                {wallet.isOpen ? (
-                  <div className={cx('networks_container')}>
-                    {wallet.networks.map((network, index) => {
-                      return (
-                        <div key={'network' + index} className={cx('network_container')}>
-                          <div>
-                            {theme === 'light' ? (
-                              network.IconLight ? (
-                                <network.IconLight className={cx('icon')} />
-                              ) : (
-                                <network.Icon className={cx('icon')} />
-                              )
+                <div className={cx('control')} onClick={() => toggleShowNetworks(wallet.id)}>
+                  {wallet.isOpen ? <UpArrowIcon /> : <DownArrowIcon />}
+                </div>
+              </div>
+              {wallet.isOpen ? (
+                <div className={cx('networks_container')}>
+                  {wallet.networks.map((network, index) => {
+                    return (
+                      <div key={'network' + index} className={cx('network_container')}>
+                        <div>
+                          {theme === 'light' ? (
+                            network.IconLight ? (
+                              <network.IconLight className={cx('icon')} />
                             ) : (
                               <network.Icon className={cx('icon')} />
-                            )}
-                          </div>
-                          <div className={cx('info')}>
-                            <div className={cx('name')}>{network.chainName}</div>
-                            {network.address ? (
-                              <div className={cx('address')}>{reduceString(network.address, 5, 5)}</div>
-                            ) : null}
-                          </div>
-                          <div className={cx('actions')}>
-                            {network.address ? (
-                              <>
-                                <div
-                                  className={cx('copy')}
-                                  onClick={(e) => copyWalletAddress(e, network.address, wallet.id, network.chainId)}
-                                >
-                                  {copiedAddressCoordinates.networkId === network.chainId &&
-                                    copiedAddressCoordinates.walletId === wallet.id ? (
-                                    <SuccessIcon width={20} height={20} />
-                                  ) : (
-                                    <CopyIcon />
-                                  )}
-                                </div>
-                                <div
-                                  className={cx('qr_code')}
-                                  onClick={() =>
-                                    getUrlQrCode({
-                                      address: network.address,
-                                      name: network.chainName,
-                                      icon: network.Icon
-                                    })
-                                  }
-                                >
-                                  <QRCodeIcon />
-                                </div>
-                              </>
-                            ) : (
-                              <div>
-                                <UnavailableCloudIcon />
-                              </div>
-                            )}
-                          </div>
+                            )
+                          ) : (
+                            <network.Icon className={cx('icon')} />
+                          )}
                         </div>
-                      );
-                    })}
-                  </div>
-                ) : null}
-              </div>
-            );
-          })}
-        </div>
-        {!isMobile() && (
-          <div className={cx('btn')} onClick={handleAddWallet}>
-            <AddIcon />
-            <div className={cx('content')}>Add Wallet</div>
-          </div>
-        )}
+                        <div className={cx('info')}>
+                          <div className={cx('name')}>{network.chainName}</div>
+                          {network.address ? (
+                            <div className={cx('address')}>{reduceString(network.address, 5, 5)}</div>
+                          ) : null}
+                        </div>
+                        <div className={cx('actions')}>
+                          {network.address ? (
+                            <>
+                              <div
+                                className={cx('copy')}
+                                onClick={(e) => copyWalletAddress(e, network.address, wallet.id, network.chainId)}
+                              >
+                                {copiedAddressCoordinates.networkId === network.chainId &&
+                                copiedAddressCoordinates.walletId === wallet.id ? (
+                                  <SuccessIcon width={20} height={20} />
+                                ) : (
+                                  <CopyIcon />
+                                )}
+                              </div>
+                              <div
+                                className={cx('qr_code')}
+                                onClick={() =>
+                                  getUrlQrCode({
+                                    address: network.address,
+                                    name: network.chainName,
+                                    icon: theme === 'light' && network.IconLight ? network.IconLight : network.Icon
+                                  })
+                                }
+                              >
+                                <QRCodeIcon />
+                              </div>
+                            </>
+                          ) : (
+                            <div>
+                              <UnavailableCloudIcon />
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              ) : null}
+            </div>
+          );
+        })}
       </div>
-    );
-  };
+      {!isMobile() && (
+        <div className={cx('btn')} onClick={handleAddWallet}>
+          <AddIcon />
+          <div className={cx('content')}>Add Wallet</div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default MyWallets;
