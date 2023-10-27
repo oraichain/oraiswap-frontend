1a79564700130a8fcc6422835167910a2840e1f8
Pham Tu <tubackkhoa@gmail.com>
2023-10-27 18:07:48 +0700

    direct duckdb-wasm

--- src/libs/duckdb.ts
+++ src/libs/duckdb.ts
@@ -1,19 +1,5 @@
 import * as duckdb from '@duckdb/duckdb-wasm';
 import { get, set } from 'idb-keyval';
-//@ts-ignore
-import duckdb_wasm from '@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm';
-//@ts-ignore
-import duckdb_wasm_next from '@duckdb/duckdb-wasm/dist/duckdb-eh.wasm';
-const MANUAL_BUNDLES: duckdb.DuckDBBundles = {
-  mvp: {
-    mainModule: duckdb_wasm,
-    mainWorker: new URL('@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js', import.meta.url).toString()
-  },
-  eh: {
-    mainModule: duckdb_wasm_next,
-    mainWorker: new URL('@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js', import.meta.url).toString()
-  }
-};
 
 export type TransactionHistory = {
   initialTxHash: string;
@@ -58,12 +44,12 @@ export class DuckDb {
 
   static async create() {
     // Select a bundle based on browser checks
-    const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
     // Instantiate the asynchronus version of DuckDB-Wasm
-    const worker = new Worker(bundle.mainWorker!);
-    const logger = new duckdb.ConsoleLogger();
-    const db = new duckdb.AsyncDuckDB(logger, worker);
-    await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+    const db = new duckdb.AsyncDuckDB(
+      new duckdb.ConsoleLogger(),
+      new Worker(new URL('@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js', import.meta.url).toString())
+    );
+    await db.instantiate(require('@duckdb/duckdb-wasm/dist/duckdb-eh.wasm'));
     const conn = await db.connect();
     DuckDb.instance = new DuckDb(conn, db);
   }
