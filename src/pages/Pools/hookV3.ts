58787376929f91949d6fd2ab58cdb79b097f7e96
sonnx-oraichain <son.nx@orai.io>
2023-10-27 14:35:25 +0700

    Update percent change chart

--- src/pages/Pools/hookV3.ts
+++ src/pages/Pools/hookV3.ts
@@ -125,6 +125,28 @@ export const getPools = async (): Promise<PoolInfoResponse[]> => {
   }
 };
 
+export const useGetPriceChange = (params: { base_denom: string; quote_denom: string; tf: number }) => {
+  const getPriceChange = async (queries: { base_denom: string; quote_denom: string; tf: number }) => {
+    try {
+      const res = await axios.get('https://api-staging.oraidex.io/price', { params: queries }); // TODO: change Server url
+      return res.data;
+    } catch (e) {
+      console.error('useGetPriceChange', e);
+      return {};
+    }
+  };
+
+  const { data: priceChange, isLoading } = useQuery(['price-change', params], () => getPriceChange(params), {
+    refetchOnWindowFocus: true,
+    staleTime: 5 * 60 * 1000
+  });
+
+  return {
+    isLoading,
+    priceChange
+  };
+};
+
 export const useGetPools = () => {
   const { data: pools } = useQuery(['pools'], getPools, {
     refetchOnWindowFocus: true,
