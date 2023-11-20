1d5d4977337738940902fd24c0c24e788061647e
quangnt <ntq.1704@gmail.com>
2023-11-20 15:27:48 +0700

    fix: resolve comment PR

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -5,19 +5,22 @@ import SearchSvg from 'assets/images/search-svg.svg';
 import SearchLightSvg from 'assets/images/search-light-svg.svg';
 import Input, { InputProps } from './Input';
 
-const Search: React.FC<InputProps> = ({ className, isBorder, theme, style, ...props }) => (
-  <Input
-    className={classNames(className, isBorder ? styles.universalSearch : styles.search)}
-    placeholder="Search by pools or tokens name"
-    style={{
-      paddingLeft: 40,
-      backgroundImage: `url(${theme === 'light' ? SearchLightSvg : SearchSvg})`,
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: '10px center',
-      ...style
-    }}
-    {...props}
-  />
-);
+const Search: React.FC<InputProps> = ({ className, isBorder, theme, style, ...props }) => {
+  const bgUrl = theme === 'light' ? SearchLightSvg : SearchSvg;
 
+  return (
+    <Input
+      className={classNames(className, isBorder ? styles.universalSearch : styles.search)}
+      placeholder="Search by pools or tokens name"
+      style={{
+        paddingLeft: 40,
+        backgroundImage: `url(${bgUrl})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: '10px center',
+        ...style
+      }}
+      {...props}
+    />
+  );
+};
 export default Search;
