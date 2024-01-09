9786bde21f5dbd40cc67e1367be4c08e62fcedbd
Toan Dang <toan.dq@orai.io>
2024-01-09 16:56:48 +0700

    update release unique

--- config-overrides.js
+++ config-overrides.js
@@ -98,10 +98,10 @@ module.exports = {
 
           // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
           // and needs the `project:releases` and `org:read` scopes
-          authToken: process.env.SENTRY_AUTH_TOKEN
+          authToken: process.env.SENTRY_AUTH_TOKEN,
 
           // Optionally uncomment the line below to override automatic release name detection
-          // release: process.env.RELEASE
+          release: vendorHash
         })
       );
     }
