b14db0596e88742ee5052e63c448ad555122f09b
Hau Nguyen Van <nguyenvanhauit1997@gmail.com>
2024-01-10 16:37:01 +0700

    fix ui

--- src/pages/CoHarvest/hooks/useCountdown.ts
+++ src/pages/CoHarvest/hooks/useCountdown.ts
@@ -13,33 +13,32 @@ export const useCountdown = (bidInfo) => {
   const [percent, setPercent] = useState(0);
   const [isEnd, setIsEnd] = useState(false);
   const countdownRef = useRef(null);
-  // const [start, end] = [new Date(Date.now()), new Date(bidInfo.end_time)];
-
+  const getTimeDateNow = Date.now();
   const [start, setStart] = useState(bidInfo?.start_time);
   const [end, setEnd] = useState(bidInfo?.end_time);
-  const [timeRemaining, setTimeRemaining] = useState(() => calcDiffTime(new Date().getTime(), end));
+  const [timeRemaining, setTimeRemaining] = useState(() => calcDiffTime(getTimeDateNow, end));
 
   useEffect(() => {
     if (!bidInfo.round) return;
     setStart(bidInfo?.start_time);
     setEnd(bidInfo?.end_time);
     // when bidInfo round === 0 => time is in milliseconds and when round != 0 time is in seconds
-    setTimeRemaining(() => calcDiffTime(new Date().getTime(), bidInfo?.end_time * TIMER.MILLISECOND));
-
-    const decrementTime = () => {
-      setTimeRemaining((prev) => {
-        const newRemain = prev - TIMER.MILLISECOND;
-        if (newRemain <= 0) {
-          clearInterval(countdownRef.current);
-          countdownRef.current = null;
-          setIsEnd(true);
-          return 0;
-        }
-        return newRemain;
-      });
-    };
-
-    countdownRef.current = setInterval(decrementTime, TIMER.MILLISECOND);
+    if (bidInfo?.end_time > getTimeDateNow) {
+      setTimeRemaining(() => calcDiffTime(getTimeDateNow, bidInfo?.end_time * TIMER.MILLISECOND));
+      const decrementTime = () => {
+        setTimeRemaining((prev) => {
+          const newRemain = prev - TIMER.MILLISECOND;
+          if (newRemain <= 0) {
+            clearInterval(countdownRef.current);
+            countdownRef.current = null;
+            setIsEnd(true);
+            return 0;
+          }
+          return newRemain;
+        });
+      };
+      countdownRef.current = setInterval(decrementTime, TIMER.MILLISECOND);
+    }
 
     return () => {
       if (countdownRef.current) {
