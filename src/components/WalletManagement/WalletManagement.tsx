d85d6bd80c2f868c21a75b8221037a85c469ff56
trungbach <trunga2k29@gmail.com>
2024-01-21 18:08:46 +0700

    chore: handle load balance when acc change

--- src/components/WalletManagement/WalletManagement.tsx
+++ src/components/WalletManagement/WalletManagement.tsx
@@ -1,23 +1,51 @@
-import cn from 'classnames/bind';
-import { FC, useState } from 'react';
+import { useWallet } from '@cosmos-kit/react';
 import { isMobile } from '@walletconnect/browser-utils';
+import cn from 'classnames/bind';
 import { Button } from 'components/Button';
+import type { Wallet as WalletResetType } from 'components/ConnectWallet/useResetBalance';
+import { useResetBalance } from 'components/ConnectWallet/useResetBalance';
 import useConfigReducer from 'hooks/useConfigReducer';
-import { ModalChooseWallet } from './ModalChooseWallet';
-import styles from './WalletManagement.module.scss';
-import { MyWallet } from './MyWallet';
+import useLoadTokens from 'hooks/useLoadTokens';
+import { FC, useEffect, useState } from 'react';
 import Connected from './Connected';
+import { ModalChooseWallet } from './ModalChooseWallet';
+import { MyWallet } from './MyWallet';
+import styles from './WalletManagement.module.scss';
 const cx = cn.bind(styles);
 
 export const WalletManagement: FC<{}> = () => {
   const [theme] = useConfigReducer('theme');
-  const [isShowChooseWallet, setIsShowChooseWallet] = useState(false);
+  const [isShowChooseWallet, setIsShowChooseWallet] = useState(true);
   const [isShowMyWallet, setIsShowMyWallet] = useState(false);
-  const isConnected = true;
+  const wallet = useWallet();
+  const loadTokenAmounts = useLoadTokens();
+  const [oraiAddress] = useConfigReducer('address');
+  const [tronAddress] = useConfigReducer('tronAddress');
+  const [metamaskAddress] = useConfigReducer('metamaskAddress');
+  const { handleResetBalance } = useResetBalance();
 
+  // load balance every time change address
+  useEffect(() => {
+    loadTokenAmounts({ oraiAddress, tronAddress, metamaskAddress });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [oraiAddress, tronAddress, metamaskAddress]);
+
+  // reset balance when disconnect
+  useEffect(() => {
+    if (!metamaskAddress || !tronAddress || !oraiAddress) {
+      let arrResetBalance: WalletResetType[] = [];
+      if (!metamaskAddress) arrResetBalance.push('metamask');
+      if (!tronAddress) arrResetBalance.push('tron');
+      if (!oraiAddress) arrResetBalance.push('keplr');
+      arrResetBalance.length && handleResetBalance(arrResetBalance);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [oraiAddress, tronAddress, metamaskAddress]);
+
+  const isAnyWalletConnected = !!wallet.mainWallet;
   return (
     <div className={cx('connect-wallet-container', theme)}>
-      {!isConnected ? (
+      {!isAnyWalletConnected ? (
         <Button
           type="primary"
           onClick={() => {
