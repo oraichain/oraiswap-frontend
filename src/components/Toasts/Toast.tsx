551a849fa9f45debaeaafb7b23bdfa00003a10c0
trungbach <trunga2k29@gmail.com>
2024-01-29 18:10:26 +0700

    chore: add toast type for wallet failed

--- src/components/Toasts/Toast.tsx
+++ src/components/Toasts/Toast.tsx
@@ -31,7 +31,8 @@ export enum TToastType {
   TX_INFO,
   KEPLR_FAILED,
   METAMASK_FAILED,
-  TRONLINK_FAILED
+  TRONLINK_FAILED,
+  WALLET_FAILED
 }
 
 interface IToastExtra {
@@ -104,7 +105,6 @@ export const displayToast: DisplayToastFn = (
     ...refinedOptions,
     closeOnClick: true
   } as ToastOptions;
-
   switch (type) {
     case TToastType.TX_INFO:
       return toast(
@@ -136,6 +136,8 @@ export const displayToast: DisplayToastFn = (
       return toast(<ToastMetamaskFailed message={inputExtraData.message} />, inputOptions);
     case TToastType.TRONLINK_FAILED:
       return toast(<ToastTronLinkFailed message={inputExtraData.message} />, inputOptions);
+    case TToastType.WALLET_FAILED:
+      return toast(<ToastWalletFailed message={inputExtraData.message} />, inputOptions);
     default:
       return console.error(`Undefined toast type - ${type}`);
   }
@@ -204,6 +206,16 @@ const ToastTronLinkFailed: FunctionComponent<{ message: string }> = ({ message }
     <FailedIcon />
     <section className={styles.toast_section}>
       <h6>Tronlink failed</h6>
+      <p>{message}</p>
+    </section>
+  </div>
+);
+
+const ToastWalletFailed: FunctionComponent<{ message: string }> = ({ message }) => (
+  <div className={styles.toast_content}>
+    <FailedIcon />
+    <section className={styles.toast_section}>
+      <h6>Wallet failed</h6>
       <p>{message}</p>
     </section>
   </div>
