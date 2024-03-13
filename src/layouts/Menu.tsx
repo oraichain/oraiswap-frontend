e4e8c6ddb21f47266a5ece30dbd3510f510595ef
perfogic <longdieu12x@gmail.com>
2024-03-13 17:21:12 +0700

    fix not create _blank tab when go to bitcoin-dashboard

--- src/layouts/Menu.tsx
+++ src/layouts/Menu.tsx
@@ -134,7 +134,7 @@ const Menu: React.FC = () => {
                   setOtherActive(!otherActive);
                 })}
                 {renderLink('https://legacy-v2.oraidex.io/', 'OraiDEX Legacy', () => {}, true)}
-                {renderLink('/bitcoin-dashboard', 'Bitcoin Dashboard', () => {}, true)}
+                {renderLink('/bitcoin-dashboard?tab=pending_deposits', 'Bitcoin Dashboard', () => {}, false)}
               </div>
             }
           />
