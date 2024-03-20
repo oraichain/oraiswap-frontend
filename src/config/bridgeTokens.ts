7dc9ec5c7da94c045b3622f6291f9e44f1720941
Hau Nguyen Van <nguyenvanhauit1997@gmail.com>
2024-03-20 16:23:48 +0700

    hotfix hide token

--- src/config/bridgeTokens.ts
+++ src/config/bridgeTokens.ts
@@ -97,33 +97,3 @@ export const kawaiiTokens = uniqBy(
   cosmosTokens.filter((token) => token.chainId === 'kawaii_6886-1'),
   (c) => c.denom
 );
-
-const notAllowSwapCoingeckoIds = ['kawaii-islands', 'milky-token', 'injective-protocol', 'bitcoin'];
-// universal swap. Currently we dont support from tokens that are not using the ibc wasm channel
-const notAllowSwapFromChainIds = [
-  'kawaii_6886-1',
-  'osmosis-1',
-  'cosmoshub-4',
-  'oraibridge-subnet-2',
-  'oraibtc-mainnet-1',
-  'injective-1',
-  'noble-1',
-  'bitcoin',
-  'Neutaro-1'
-];
-export const swapFromTokens = flattenTokens.filter(
-  (token) => !notAllowSwapCoingeckoIds.includes(token.coinGeckoId) && !notAllowSwapFromChainIds.includes(token.chainId)
-);
-// universal swap. We dont support kwt & milky & injective for simplicity. We also skip OraiBridge tokens because users dont care about them
-
-const notAllowSwapToChainIds = [
-  'oraibridge-subnet-2',
-  'oraibtc-mainnet-1',
-  'injective-1',
-  'noble-1',
-  'bitcoin',
-  'Neutaro-1'
-];
-export const swapToTokens = flattenTokens.filter(
-  (token) => !notAllowSwapCoingeckoIds.includes(token.coinGeckoId) && !notAllowSwapToChainIds.includes(token.chainId)
-);
