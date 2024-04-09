ef8a29c0c12af0c08466e1e6abdbd274b720ff6d
trungbach <trunga2k29@gmail.com>
2024-04-09 17:45:39 +0700

    chore: add vault routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,15 +3,16 @@ import Loader from 'components/Loader';
 import NotFound from 'pages/NotFound';
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-
 import Balance from 'pages/Balance';
 import Pools from 'pages/Pools';
 import PoolDetail from 'pages/Pools/PoolDetail';
-import UniversalSwap from 'pages/UniversalSwap/index';
+import UniversalSwap from 'pages/UniversalSwap';
 import CoHarvest from 'pages/CoHarvest';
 import BitcoinDashboard from 'pages/BitcoinDashboard';
 import StakingPage from 'pages/Staking';
 import DownloadApp from 'pages/DownloadApp';
+import { Vaults } from 'pages/Vaults';
+import { VaultDetail } from 'pages/VaultDetail';
 
 export default () => (
   <Suspense
@@ -37,6 +38,8 @@ export default () => (
       <Route path="/pools" element={<Pools />} />
       <Route path="/staking" element={<StakingPage />} />
       <Route path="/co-harvest" element={<CoHarvest />} />
+      <Route path="/vaults" element={<Vaults />} />
+      <Route path="/vaults/:vaultUrl" element={<VaultDetail />} />
       <Route path="/download-owallet" element={<DownloadApp />} />
       <Route path="/pools/:poolUrl" element={<PoolDetail />} />
       <Route path="*" element={<NotFound />} />
