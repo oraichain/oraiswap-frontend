83ec70688563ec7805f3349c727985a23feca34b
Hau Nguyen Van <nguyenvanhauit1997@gmail.com>
2024-04-12 18:00:08 +0700

    add banner dao

--- src/components/FutureCompetitionModal/index.tsx
+++ src/components/FutureCompetitionModal/index.tsx
@@ -1,4 +1,6 @@
-import Banner from 'assets/images/ftc_deadline_extended.png';
+// import Banner from 'assets/images/ftc_deadline_extended.png';
+import Banner from 'assets/images/oraidex_proposal_f3.png';
+
 // import BannerDark from 'assets/images/competiton-future-banner-dark.png';
 import useConfigReducer from 'hooks/useConfigReducer';
 import { useEffect, useState } from 'react';
@@ -23,10 +25,12 @@ const FutureCompetition = () => {
     <div className={`${styles.competition} ${active ? styles.active : ''}`}>
       <img src={Banner} alt="" />
       <div className={styles.content}>
-        <div className={styles.header}>Futures Trading Competition is live! 🎁</div>
-        <div className={styles.desc}>
+        {/* <div className={styles.header}>Futures Trading Competition is live! 🎁</div> */}
+        <div className={styles.header}>Our very first proposal is live! ⭐️</div>
+        {/* <div className={styles.desc}>
           Join now and showcase your trading skills for a chance to win big from the $15,000 USD prize pool!
-        </div>
+        </div> */}
+        <div className={styles.desc}>Your voice matters - Cast your vote now.</div>
         <div className={styles.btnGroup}>
           <button
             onClick={() => {
@@ -35,7 +39,13 @@ const FutureCompetition = () => {
           >
             Skip
           </button>
-          <a className={styles.learnMore} href="https://futures.oraidex.io" target="_blank" rel="noreferrer">
+          <a
+            className={styles.learnMore}
+            href="https://daodao.zone/dao/orai1y7z3gw0al5lx9yygs800zfvnkr065xjrpld9wt6950y897grppxsrf4zcr/home
+"
+            target="_blank"
+            rel="noreferrer"
+          >
             Learn more
           </a>
         </div>
