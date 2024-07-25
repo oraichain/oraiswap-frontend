25154a36046dcbac0f0c11278e74379bf9d1d06c
quangdz1704 <Ntq.1704@gmail.com>
2024-07-25 14:06:48 +0700

    feat: layout create

--- src/pages/Pool-V3/components/CreatePosition/index.tsx
+++ src/pages/Pool-V3/components/CreatePosition/index.tsx
@@ -1,7 +1,33 @@
+import classNames from 'classnames';
+import { ReactComponent as BackIcon } from 'assets/icons/back.svg';
+import { ReactComponent as SettingIcon } from 'assets/icons/setting.svg';
 import styles from './index.module.scss';
+import { useNavigate } from 'react-router-dom';
+import useTheme from 'hooks/useTheme';
 
 const CreatePosition = () => {
-  return <div className={styles.createPosition}>CreatePosition</div>;
+  const navigate = useNavigate();
+  const theme = useTheme();
+
+  return (
+    <div className={classNames('small_container', styles.createPosition)}>
+      <div className={styles.box}>
+        <div className={styles.header}>
+          <div className={styles.back} onClick={() => navigate('/pools-v3')}>
+            <BackIcon />
+          </div>
+          <h1>Add new liquidity position</h1>
+          <div className={styles.setting}>
+            <SettingIcon />
+          </div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.item}>TOKEN section</div>
+          <div className={styles.item}>PRICE section</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePosition;
