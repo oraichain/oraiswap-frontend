91c468aee91c971d9762fce0a4de98c5210d98b6
vuonghuuhung <vuonghuuhung2002@gmail.com>
2024-07-26 00:29:05 +0700

    add price range template

--- src/pages/Pool-V3/components/CreatePosition/index.tsx
+++ src/pages/Pool-V3/components/CreatePosition/index.tsx
@@ -1,13 +1,131 @@
 import classNames from 'classnames';
 import { ReactComponent as BackIcon } from 'assets/icons/back.svg';
 import { ReactComponent as SettingIcon } from 'assets/icons/setting.svg';
+import { ReactComponent as Continuous } from 'assets/images/continuous.svg';
+import { ReactComponent as Discrete } from 'assets/images/discrete.svg';
 import styles from './index.module.scss';
 import { useNavigate } from 'react-router-dom';
 import useTheme from 'hooks/useTheme';
+import PriceRangePlot from '../PriceRangePlot/PriceRangePlot';
+import { useEffect, useState } from 'react';
+import { calcPrice, calcTicksAmountInRange, spacingMultiplicityGte } from '../PriceRangePlot/utils';
+// import { getMinTick } from 'pages/Pool-V3/packages/wasm/oraiswap_v3_wasm.js';
+
+const args = {
+  currentPrice: 10000,
+  data: [
+    {
+      index: 1,
+      x: 1,
+      y: 1
+    }
+  ],
+  initialIsDiscreteValue: false,
+  leftRange: {
+    index: 2,
+    x: 10000
+  },
+  rightRange: {
+    index: 2,
+    x: 10000
+  },
+  max: 100,
+  min: 0,
+  midPrice: {
+    x: 1,
+    index: 1
+  },
+  onDiscreteChange: () => {},
+  reloadHandler: () => {},
+  ticksLoading: false,
+  tickSpacing: 1,
+  tokenX: {
+    name: 'BTC',
+    decimal: 9
+  },
+  tokenY: {
+    name: 'ETH',
+    decimal: 12
+  },
+  xToY: true,
+  hasTicksError: false
+};
 
 const CreatePosition = () => {
   const navigate = useNavigate();
   const theme = useTheme();
+
+  const [plotMin, setPlotMin] = useState(0);
+  const [plotMax, setPlotMax] = useState(1);
+
+  const [isPlotDiscrete, setIsPlotDiscrete] = useState(false);
+
+  const currentPrice = 10000;
+  const leftRange = {
+    index: 2,
+    x: 10000
+  };
+  const rightRange = {
+    index: 2,
+    x: 10000
+  };
+  const midPrice = {
+    index: 2,
+    x: 1020
+  };
+  const tokenX = {
+    name: 'BTC',
+    decimal: 9
+  };
+  const tokenY = {
+    name: 'ETH',
+    decimal: 12
+  };
+
+  useEffect(() => {
+    // const initSideDist = Math.abs(
+    //   leftRange.x -
+    //     calcPrice(
+    //       Math.max(
+    //         spacingMultiplicityGte(Number(getMinTick(Number(args.tickSpacing))), Number(args.tickSpacing)),
+    //         Number(leftRange.index) - Number(args.tickSpacing) * 15
+    //       ),
+    //       args.xToY,
+    //       tokenX.decimal,
+    //       tokenY.decimal
+    //     )
+    // );
+    // setPlotMin(leftRange.x - initSideDist);
+    // setPlotMax(rightRange.x + initSideDist);
+  }, [args.ticksLoading, leftRange, rightRange]);
+
+  const zoomMinus = () => {
+    const diff = plotMax - plotMin;
+    const newMin = plotMin - diff / 4;
+    const newMax = plotMax + diff / 4;
+    setPlotMin(newMin);
+    setPlotMax(newMax);
+  };
+
+  const zoomPlus = () => {
+    const diff = plotMax - plotMin;
+    const newMin = plotMin + diff / 6;
+    const newMax = plotMax - diff / 6;
+
+    if (
+      calcTicksAmountInRange(
+        Math.max(newMin, 0),
+        newMax,
+        Number(args.tickSpacing),
+        args.xToY,
+        Number(tokenX.decimal),
+        Number(tokenY.decimal)
+      ) >= 4
+    ) {
+      setPlotMin(newMin);
+      setPlotMax(newMax);
+    }
+  };
 
   return (
     <div className={classNames('small_container', styles.createPosition)}>
@@ -23,7 +141,95 @@ const CreatePosition = () => {
         </div>
         <div className={styles.content}>
           <div className={styles.item}>TOKEN section</div>
-          <div className={styles.item}>PRICE section</div>
+          <div className={styles.item}>
+            <div className={styles.wrapper}>
+              <div className={styles.itemTitleWrapper}>
+                <p className={styles.itemTitle}>Price Range</p>
+                <p className={styles.liquidityActive}>
+                  Active Liquidity <span className={styles.activeLiquidityIcon}>i</span>
+                </p>
+              </div>
+              <div className={styles.itemSwitcherWrapper}>
+                <div className={styles.switcherContainer}>
+                  <div className={styles.singleTabClasses}>
+                    <div className={styles.continuous}>
+                      <Continuous />
+                    </div>
+                  </div>
+                  <div className={styles.singleTabClasses}>
+                    <div className={styles.discrete}>
+                      <Discrete />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.itemChartAndPriceWrapper}>
+              <div>
+                Chart
+                {/* <PriceRangePlot
+                data={args.data}
+                plotMin={args.min}
+                plotMax={args.max}
+                zoomMinus={zoomMinus}
+                zoomPlus={zoomPlus}
+                disabled
+                leftRange={leftRange}
+                rightRange={rightRange}
+                midPrice={midPrice}
+                className={styles.plot}
+                loading={args.ticksLoading}
+                isXtoY={args.xToY}
+                tickSpacing={args.tickSpacing}
+                xDecimal={tokenX.decimal}
+                yDecimal={tokenY.decimal}
+                isDiscrete={isPlotDiscrete}
+                coverOnLoading
+                hasError={args.hasTicksError}
+                reloadHandler={args.reloadHandler}
+              /> */}
+              </div>
+
+              <div className={styles.currentPriceWrapper}>
+                <div className={styles.currentPriceTitle}>
+                  <p>Current Price</p>
+                </div>
+                <div className={styles.currentPriceValue}>
+                  <p>
+                    <p>0.081242</p>
+                    <p className={styles.pair}>ORAI / USDT</p>
+                  </p>
+                </div>
+              </div>
+
+              <div className={styles.minMaxPriceWrapper}>
+                <div className={styles.minMaxPrice}>
+                  <div className={styles.minMaxPriceTitle}>
+                    <p>Min Price</p>
+                  </div>
+                  <div className={styles.minMaxPriceValue}>
+                    <p>
+                      <p>0.081242</p>
+                      <p className={styles.pair}>ORAI / USDT</p>
+                    </p>
+                  </div>
+                </div>
+
+                <div className={styles.minMaxPrice}>
+                  <div className={styles.minMaxPriceTitle}>
+                    <p>Max Price</p>
+                  </div>
+                  <div className={styles.minMaxPriceValue}>
+                    <p>
+                      <p>0.081242</p>
+                      <p className={styles.pair}>ORAI / USDT</p>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/pages/Pool-V3/components/PriceRangePlot/utils.ts
@@ -0,0 +1,182 @@
+import {
+  TokenAmount,
+  SwapError,
+  alignTickToSpacing,
+  calculateSqrtPrice,
+  getMaxTick,
+  getMinTick,
+//   getPriceScale
+} from 'pages/Pool-V3/packages/wasm/oraiswap_v3_wasm.js';
+
+export const PRICE_SCALE = 24;
+export const CONCENTRATION_FACTOR = 1.00001526069123;
+
+export const getPrimaryUnitsPrice = (price: number, isXtoY: boolean, xDecimal: number, yDecimal: number) => {
+  const xToYPrice = isXtoY ? price : 1 / price;
+
+  return xToYPrice * 10 ** (yDecimal - xDecimal);
+};
+
+export const logBase = (x: number, b: number): number => Math.log(x) / Math.log(b);
+
+export const adjustToSpacing = (baseTick: number, spacing: number, isGreater: boolean): number => {
+  const remainder = baseTick % spacing;
+
+  if (Math.abs(remainder) === 0) {
+    return baseTick;
+  }
+
+  let adjustment: number;
+  if (isGreater) {
+    if (baseTick >= 0) {
+      adjustment = spacing - remainder;
+    } else {
+      adjustment = Math.abs(remainder);
+    }
+  } else {
+    if (baseTick >= 0) {
+      adjustment = -remainder;
+    } else {
+      adjustment = -(spacing - Math.abs(remainder));
+    }
+  }
+
+  return baseTick + adjustment;
+};
+
+export const spacingMultiplicityLte = (arg: number, spacing: number): number => {
+  return adjustToSpacing(arg, spacing, false);
+};
+
+export const spacingMultiplicityGte = (arg: number, spacing: number): number => {
+  return adjustToSpacing(arg, spacing, true);
+};
+
+export const nearestSpacingMultiplicity = (centerTick: number, spacing: number) => {
+  const greaterTick = spacingMultiplicityGte(centerTick, spacing);
+  const lowerTick = spacingMultiplicityLte(centerTick, spacing);
+
+  const nearestTick = Math.abs(greaterTick - centerTick) < Math.abs(lowerTick - centerTick) ? greaterTick : lowerTick;
+
+  return Math.max(Math.min(nearestTick, Number(getMaxTick(spacing))), Number(getMinTick(spacing)));
+};
+
+export const getTickAtSqrtPriceFromBalance = (
+  price: number,
+  spacing: number,
+  isXtoY: boolean,
+  xDecimal: number,
+  yDecimal: number
+) => {
+  const minTick = getMinTick(spacing);
+  const maxTick = getMaxTick(spacing);
+
+  const basePrice = Math.max(price, Number(calcPrice(isXtoY ? minTick : maxTick, isXtoY, xDecimal, yDecimal)));
+  const primaryUnitsPrice = getPrimaryUnitsPrice(basePrice, isXtoY, Number(xDecimal), Number(yDecimal));
+  const tick = Math.round(logBase(primaryUnitsPrice, 1.0001));
+
+  return Math.max(Math.min(tick, Number(getMaxTick(spacing))), Number(getMinTick(spacing)));
+};
+
+export const calcPrice = (amountTickIndex: number, isXtoY: boolean, xDecimal: number, yDecimal: number): number => {
+  const price = calcYPerXPriceByTickIndex(amountTickIndex, xDecimal, yDecimal);
+
+  return isXtoY ? price : 1 / price;
+};
+
+export const calcYPerXPriceByTickIndex = (tickIndex: number, xDecimal: number, yDecimal: number): number => {
+  const sqrt = +printBigint(calculateSqrtPrice(tickIndex), PRICE_SCALE);
+
+  const proportion = sqrt * sqrt;
+
+  return proportion / 10 ** (yDecimal - xDecimal);
+};
+
+export const printBigint = (amount: TokenAmount, decimals: number): string => {
+  const amountString = amount.toString();
+  const isNegative = amountString.length > 0 && amountString[0] === '-';
+
+  const balanceString = isNegative ? amountString.slice(1) : amountString;
+
+  if (balanceString.length <= decimals) {
+    return (isNegative ? '-' : '') + '0.' + '0'.repeat(decimals - balanceString.length) + balanceString;
+  } else {
+    return (
+      (isNegative ? '-' : '') +
+      trimZeros(
+        balanceString.substring(0, balanceString.length - decimals) +
+          '.' +
+          balanceString.substring(balanceString.length - decimals)
+      )
+    );
+  }
+};
+
+export const trimZeros = (numStr: string): string => {
+  return numStr
+    .replace(/(\.\d*?)0+$/, '$1')
+    .replace(/^0+(\d)|(\d)0+$/gm, '$1$2')
+    .replace(/\.$/, '');
+};
+
+export const nearestTickIndex = (
+  price: number,
+  spacing: number,
+  isXtoY: boolean,
+  xDecimal: number,
+  yDecimal: number
+) => {
+  const tick = getTickAtSqrtPriceFromBalance(price, spacing, isXtoY, xDecimal, yDecimal);
+
+  return nearestSpacingMultiplicity(tick, spacing);
+};
+
+export const getConcentrationArray = (tickSpacing: number, minimumRange: number, currentTick: number): number[] => {
+  const concentrations: number[] = [];
+  let counter = 0;
+  let concentration = 0;
+  let lastConcentration = calculateConcentration(tickSpacing, minimumRange, counter) + 1;
+  let concentrationDelta = 1;
+
+  while (concentrationDelta >= 1) {
+    concentration = calculateConcentration(tickSpacing, minimumRange, counter);
+    concentrations.push(concentration);
+    concentrationDelta = lastConcentration - concentration;
+    lastConcentration = concentration;
+    counter++;
+  }
+  concentration = Math.ceil(concentrations[concentrations.length - 1]);
+
+  while (concentration > 1) {
+    concentrations.push(concentration);
+    concentration--;
+  }
+  const maxTick = alignTickToSpacing(getMaxTick(1), tickSpacing);
+  if ((minimumRange / 2) * tickSpacing > maxTick - Math.abs(currentTick)) {
+    throw new Error(String(SwapError.TickLimitReached));
+  }
+  const limitIndex = (maxTick - Math.abs(currentTick) - (minimumRange / 2) * tickSpacing) / tickSpacing;
+
+  return concentrations.slice(0, limitIndex);
+};
+
+export const calculateConcentration = (tickSpacing: number, minimumRange: number, n: number) => {
+  const concentration = 1 / (1 - Math.pow(1.0001, (-tickSpacing * (minimumRange + 2 * n)) / 4));
+  return concentration / CONCENTRATION_FACTOR;
+};
+
+export const calcTicksAmountInRange = (
+  min: number,
+  max: number,
+  tickSpacing: number,
+  isXtoY: boolean,
+  xDecimal: number,
+  yDecimal: number
+): number => {
+  const primaryUnitsMin = getPrimaryUnitsPrice(min, isXtoY, xDecimal, yDecimal);
+  const primaryUnitsMax = getPrimaryUnitsPrice(max, isXtoY, xDecimal, yDecimal);
+  const minIndex = logBase(primaryUnitsMin, 1.0001);
+  const maxIndex = logBase(primaryUnitsMax, 1.0001);
+
+  return Math.ceil(Math.abs(maxIndex - minIndex) / tickSpacing);
+};
