ad7635a37dd4187e9debb4fd58c180c25d9b359d
Trung Bạch <66735837+trungbach@users.noreply.github.com>
2024-08-24 18:33:09 +0700

    redux: increase persist version (#930)

--- src/store/constants.ts
+++ src/store/constants.ts
@@ -1,5 +1,5 @@
 // change version when update, add state redux-persist storage
-export const PERSIST_VER = 2.1;
+export const PERSIST_VER = 2.2;
 
 export const PERSIST_CONFIG_KEY = 'root';
 export const ADDRESS_BOOK_KEY_BACKUP = 'addressBookBackup';
